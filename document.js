4ea376582602d133ab6a932fa78028b18c2aff96
Bombondos <107577006+Bombondos@users.noreply.github.com>
2022-08-03 14:51:32 +0300

    Add files via upload

---
+++ document.js
@@ -0,0 +1,6 @@
+function bebrash() {
+	var username = document.getElementById("B1").value
+	var password = document.getElementById("B2").value
+	var govno = document.getElementById("h228")
+	if(username == "B1" && password == "B2"){govno.innerHTML=("говно")}
+}
